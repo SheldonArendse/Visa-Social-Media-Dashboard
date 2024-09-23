b5c20a361469727de933ab3e3275e027afdc2d08
SheldonArendse <sheldonarendse0101@gmail.com>
2024-09-23 16:02:34 +0200

    removed title field from dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -79,11 +79,6 @@
                         <form action="{{ url('/facebook/post') }}" method="POST" id="article-form" enctype="multipart/form-data">
                             @csrf
                             <div class="mb-4">
-                                <label for="title" class="block text-sm font-medium text-gray-700">Title</label>
-                                <input type="text" name="title" id="title" class="mt-1 focus:ring-secondary focus:border-secondary block w-full shadow-sm sm:text-sm border-gray-400 rounded-md px-3 py-2">
-                            </div>
-
-                            <div class="mb-4">
                                 <label for="content" class="block text-sm font-medium text-gray-700">Content</label>
                                 <textarea name="content" id="content" rows="5" class="mt-1 focus:ring-secondary focus:border-secondary block w-full shadow-sm sm:text-sm border-gray-400 rounded-md px-3 py-2" placeholder="What's on your mind?"></textarea>
                             </div>
